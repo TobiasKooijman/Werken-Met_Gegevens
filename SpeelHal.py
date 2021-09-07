20d09a59a6dca578107726d3397de1b5a54f9a71
Tobias Kooijman <99068704@mydavinci.nl>
2021-09-07 11:23:27 +0200

    Stokbrood en Speelhal opdrachten toegevoegd

---
+++ SpeelHal.py
@@ -0,0 +1,18 @@
+# Variables
+
+Ticket_PP = 7.45
+Aantal_Mensen = 4
+VIP_Gameseat_Pr5 = 0.37
+Tijd_Gameseat = 4
+Ticket_totaal = (Ticket_PP*Aantal_Mensen)
+Gameseat_Totaal = (VIP_Gameseat_Pr5*Tijd_Gameseat)
+
+
+# Sommen en Prints en shit
+
+print("Prijs Tickets")
+print(Ticket_PP*Aantal_Mensen)
+print("Prijs VR Gameseat:")
+print(VIP_Gameseat_Pr5*Tijd_Gameseat)
+print("Totale Bedrag:")
+print(Ticket_totaal+Gameseat_Totaal)
---
+++ Stokbrood.py
@@ -0,0 +1,15 @@
+# Value Numbers:
+Croissant = 0.39
+Aantal_Croissant = 17
+Stokbrood = 2.78
+Aantal_Stokbrood = 2
+Totaal_Stokbrood = (Stokbrood*Aantal_Stokbrood)
+Totaal_Croissant = (Croissant*Aantal_Croissant)
+
+# Prints en shit
+print("Prijs Croissant")
+print(Croissant*Aantal_Croissant)
+print("Prijs Stokbrood:")
+print(Stokbrood*Aantal_Stokbrood)
+print("Totale Bedrag:")
+print(Totaal_Croissant+Totaal_Stokbrood)
